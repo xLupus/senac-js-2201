693f1df680c903151689b1386ed10e55602e6dbc
VINICIUS DO AMARAL SOUZA <vinicius.asouza34@senacsp.edu.br>
2022-02-16 22:11:54 -0300

    Review do semestre passado - parte 1

---
+++ app.js
@@ -0,0 +1,28 @@
+//comentario de linha
+
+/*
+Comentario em bloco
+*/
+
+const XPTO = 10;
+console.log(XPTO);
+
+let minhaVar = "Ola";
+minhaVar = minhaVar + " Mundo";
+console.log(minhaVar);
+
+function testeDoCodigo(){
+
+    if(minhaVar != "Ola")
+        console.log("Ta diferente agr");
+
+    for(let i = 1; i <= 5; i++)
+        console.log(i);
+
+}
+
+testeDoCodigo();
+
+function diretoNoElemento(){
+    console.log('Olá mundo!!');
+}
