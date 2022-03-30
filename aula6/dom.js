8b25f1a84026751eab359ab785c35a9969d1c650
Vinicius <vinicius.am.souza221b@gmail.com>
2022-03-30 14:59:23 -0300

    corrigindo a duplicaçao de pastas

--- aula6/dom.js
+++ aula6/dom.js
@@ -1,32 +1,21 @@
-const p = document.getElementById('paragrafo');
-const titulo = document.querySelector('p');
+//Manipulação de Elemento
+const ITENS = document.querySelector('ul.collection');
 
-console.log(titulo);
-p.innerText = 'Meu texto!';
+let itens = ITENS.children[0].firstChild.textContent = 'uai';
 
-if(confirm('Quer entra com um valor?')){
+let text = document.getElementsByClassName('input-field');
 
-    let valor = prompt('Entre com um número de 0 a 10');
+let texto = text[0].children[1].textContent = 'Do balacobacu';
 
-    if( valor >= 0 && valor <= 10){
+console.log(texto);
 
-        for(let i = 0; i <= valor; i++){
-            let oi = p.innerText = "oi = " + i;
-            //42
-            //document.write(oi + "<br>");
-            console.log(oi);
-            
-        }
 
-        titulo.style.backgroundColor = 'brown';
-    } else {
+//const IT = document.querySelector('form div label');
 
-        p.innerText = 'Valor inválido';
+//children-parents-sibling
+//childElementCount
 
-        titulo.style.backgroundColor = 'yellow';
-    }
+const BOTON = document.getElementsByClassName('clear-tasks btn black');
 
-} else {
-    p.innerText = 'Que pena que você não entrou com um valor :-(';
-    titulo.style.backgroundColor = 'purple';
-}
+console.log(BOTON[0].textContent = 'SEM NADA PRA FAZER');
+
