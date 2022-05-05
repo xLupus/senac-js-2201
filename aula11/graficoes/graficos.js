750c134dafe761a53abd9a19380e0869114e6f60
VINICIUS DO AMARAL SOUZA <vinicius.asouza34@senacsp.edu.br>
2022-05-04 22:02:42 -0300

    Api de grafico, fetch

---
+++ aula11/graficoes/graficos.js
@@ -0,0 +1,68 @@
+
+new Morris.Line({
+
+    element: 'linha',
+
+    data: [
+      { year: '2008', value: 20 },
+      { year: '2009', value: 10 },
+      { year: '2010', value: 5 },
+      { year: '2011', value: 5 },
+      { year: '2012', value: 20 }
+    ],
+
+    xkey: 'year',
+    ykeys: ['value'],
+    labels: ['Value']
+  });
+
+
+  Morris.Bar({
+    element: 'bar',
+
+    data: [
+      { y: '2006', a: 100, b: 90 },
+      { y: '2007', a: 75,  b: 65 },
+      { y: '2008', a: 50,  b: 40 },
+      { y: '2009', a: 75,  b: 65 },
+      { y: '2010', a: 50,  b: 40 },
+      { y: '2011', a: 75,  b: 65 },
+      { y: '2012', a: 100, b: 90 }
+    ],
+
+    xkey: 'y',
+    ykeys: ['a', 'b'],
+    labels: ['Series A', 'Series B']
+  });
+
+
+  Morris.Area({
+
+    element: 'area',
+
+    data: [
+      { y: '2006', a: 100, b: 90 },
+      { y: '2007', a: 75,  b: 65 },
+      { y: '2008', a: 50,  b: 40 },
+      { y: '2009', a: 75,  b: 65 },
+      { y: '2010', a: 50,  b: 40 },
+      { y: '2011', a: 75,  b: 65 },
+      { y: '2012', a: 100, b: 90 }
+    ],
+
+    xkey: 'y',
+    ykeys: ['a', 'b'],
+    labels: ['Series A', 'Series B']
+  });
+
+
+Morris.Donut({
+
+  element: 'donut',
+
+  data: [
+    {label: "Download Sales", value: 12},
+    {label: "In-Store Sales", value: 30},
+    {label: "Mail-Order Sales", value: 20}
+  ]
+});
